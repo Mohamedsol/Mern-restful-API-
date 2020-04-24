f2e7bd3f9d482e129cd1df146631e2d002b3d8b6
Mohamedsol <55587981+Mohamedsol@users.noreply.github.com>
2020-04-24 01:34:14 -0500

    Update app.js

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use((error, req, res, next) => {
   res.json({message: error.message || 'An unknown error occurred!'});
 });
 
-mongoose.connect('url')
+mongoose.connect('mongodb+srv://Mohamed:99660010@mern-gf48k.mongodb.net/places?retryWrites=true&w=majority')
 .then(() => {
   app.listen(5000);
   console.log('db is connected')
